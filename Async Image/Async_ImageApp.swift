9513ac8cdf4be3d1b27918c759b710518202be16
deepak <deepakkumardash313@gmail.com>
2022-04-12 23:01:27 +0530

    Initial Commit

---
+++ Async Image/Async_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_ImageApp.swift
+//  Async Image
+//
+//  Created by Deepak kumar Dash on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Async_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
