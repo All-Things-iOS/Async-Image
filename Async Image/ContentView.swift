8f69327032c94d46e2f6f4695b9966c097fb05e6
deepak <deepakkumardash313@gmail.com>
2022-04-12 23:39:35 +0530

    creating reusable image modifiers

--- Async Image/ContentView.swift
+++ Async Image/ContentView.swift
@@ -6,16 +6,31 @@
 //
 
 import SwiftUI
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth:128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
 
 struct ContentView: View {
     private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
     var body: some View {
-//        Basic image view
-//       AsyncImage(url: URL(string: imageURL))
-        
-//     Scaling images
-        AsyncImage(url: URL(string: imageURL), scale: 3.0)
-        
+        AsyncImage(url: URL(string: imageURL), scale: 2.0) {
+            image in image.imageModifier()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill").iconModifier()
+        }
+        .padding(40)
     }
 }
 
